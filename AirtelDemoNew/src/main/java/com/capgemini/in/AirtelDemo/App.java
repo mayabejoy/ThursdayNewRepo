5e2db85f7291f0b580c89d508e25466f5e65c636
Maya Bejoy <mayabejoy@gmail.com>
2022-11-10 09:42:16 +0530

    Thursday new commit

---
+++ AirtelDemoNew/src/main/java/com/capgemini/in/AirtelDemo/App.java
@@ -0,0 +1,13 @@
+package com.capgemini.in.AirtelDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
